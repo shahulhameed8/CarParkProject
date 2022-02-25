f1e25c7388a15057a870f7c9260b64dc25c178a9
shahulhameed8 <shahul.hameed8@gmail.com>
2022-02-25 15:45:01 +0300

    CarParking

---
+++ ParkingSpacePicker.py
@@ -0,0 +1,40 @@
+import cv2
+import pickle
+
+
+# img = cv2.imread('carParking.png')
+
+
+width, height = 107, 48
+
+try:
+    with open('CarParkPos', 'rb') as f:
+        posList = pickle.load(f)
+except:
+    posList = []
+
+
+def mouseClick(events, x, y, flags, params):
+    if events == cv2.EVENT_RBUTTONDOWN:
+        posList.append((x, y))
+    if events == cv2.EVENT_LBUTTONDOWN:
+        for i, pos in enumerate(posList):
+            x1, y1 = pos
+            if x1 < x < x1 + width and y1 < y < y1 + height:
+                posList.pop(i)
+
+    with open('CarParkPos', 'wb') as f:
+        pickle.dump(posList, f)
+
+
+while True:
+    img = cv2.imread('carParking.png')
+    for pos in posList:
+        cv2.rectangle(
+            img, pos, (pos[0]+width, pos[1]+height), (255, 0, 255), 2)
+
+    cv2.imshow("image", img)
+    cv2.setMouseCallback("image", mouseClick)
+    cv2.waitKey(1)
+
+
